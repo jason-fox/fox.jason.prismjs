649da379865c1e6d78061e4a5e54bcaa2f38cc54
Jason Fox <jason.fox@fiware.org>
2026-04-12 13:23:29 +0200

    Release Preparation

--- test/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# This file is part of the DITA-OT Prism-JS Plug-in project.
-set -e
-
-apt-get update -q
-export DEBIAN_FRONTEND=noninteractive
-apt-get install -qy --no-install-recommends nodejs
-nodejs -v
